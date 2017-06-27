8ffadf88c891f9a50c6bf5538464eadb5462b64a
Eddie Fang <efang@housecanary.com>
2017-06-27 13:26:47 -0700

    Misc

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 // @flow
 import type { ViewAction } from './view.actions';
-//import type { AcceptOrderAction } from './accept-order.actions';
 import type { UserAction } from './user.actions';
 import type { OrderAction } from './order.actions';
 
